d170e68012f2c90eb19769587a728c48663528a3
Sidd Rao <siddrao.926@gmail.com>
2021-04-03 07:43:40 -0700

    Added Slow Mode And Auto Aim

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpMain.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.OpModes.TeleOp;
 
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.control.PIDFController;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -41,7 +43,7 @@ public class TeleOpMain extends LinearOpMode {
 
         if(isStopRequested()) return;
 
-        driveBase.setPoseEstimate(PoseStorage.currentPose);
+        //driveBase.setPoseEstimate(PoseStorage.currentPose);
 
         shooter.Unkick();
         shooter.SetShooter(Constants.SHOOTER_VELOCITY);
@@ -50,31 +52,48 @@ public class TeleOpMain extends LinearOpMode {
         {
             Pose2d poseEstimate = driveBase.getPoseEstimate();
 
-            Vector2d input = new Vector2d(
-                    -gamepad1.left_stick_y,
-                    -gamepad1.left_stick_x
-            ).rotated(-poseEstimate.getHeading());
+            if(!gamepad1.right_bumper)
+            {
+                Vector2d input = new Vector2d(
+                        -gamepad1.left_stick_y,
+                        -gamepad1.left_stick_x
+                ).rotated(-poseEstimate.getHeading());
 
-            driveBase.setWeightedDrivePower(
-                    new Pose2d(
-                            input.getX(),
-                            input.getY(),
-                            -gamepad1.right_stick_x
-                    )
-            );
+                driveBase.setWeightedDrivePower(
+                        new Pose2d(
+                                input.getX(),
+                                input.getY(),
+                                -gamepad1.right_stick_x
+                        )
+                );
+            }
+
+            else {
+                Vector2d input = new Vector2d(
+                        -gamepad1.left_stick_y/2,
+                        -gamepad1.left_stick_x/2
+                ).rotated(-poseEstimate.getHeading());
+
+                driveBase.setWeightedDrivePower(
+                        new Pose2d(
+                                input.getX(),
+                                input.getY(),
+                                -gamepad1.right_stick_x/2
+                        )
+                );
+            }
+
+
 
             driveBase.update();
 
             if(gamepad1.a)
             {
-                /*poseEstimate = driveBase.getPoseEstimate();
+                poseEstimate = driveBase.getPoseEstimate();
 
                 double angleToGoal = Math.atan2(Constants.GOAL_VECTOR2D.getX() - poseEstimate.getX(), Constants.GOAL_VECTOR2D.getY() - poseEstimate.getY());
-                Trajectory aimToGoalTrajectory = driveBase.trajectoryBuilder(new Pose2d())
-                    .lineToLinearHeading(new Pose2d(poseEstimate.getX(), poseEstimate.getY(), angleToGoal))
-                    .build();
 
-                driveBase.followTrajectory(aimToGoalTrajectory);*/
+                driveBase.turn(angleToGoal);
 
                 for(int i = 0; i <3; i++)
                 {
