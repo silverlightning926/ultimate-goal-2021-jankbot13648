1e50bd8dd9b989d53b0f5f81ebda8848b7296a21
Sidd Rao <siddrao.926@gmail.com>
2021-04-04 12:16:00 -0700

    Tuned Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveBase/drive/DriveConstants.java
@@ -42,7 +42,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.016;
+    public static double kV = 0.023;
     public static double kA = 0.0023;
     public static double kStatic = 0.06520;
 
