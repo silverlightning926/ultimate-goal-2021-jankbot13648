0b6ff93f7e5e2c28ffa47f553c3858aad3b5bc02
Samarth Rao <raosamarth91@gmail.com>
2021-04-13 12:21:57 -0700

    Made Shooter Speed Only Variable in TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOp/TeleOpMain.java
@@ -99,7 +99,7 @@ public class TeleOpMain extends LinearOpMode {
                 PowerShotTrajectory();
             }
 
-            shooter.setShooter((84 - driveBase.getPoseEstimate().getX())/24);
+            shooter.autoSetShooter((84 - driveBase.getPoseEstimate().getX())/24);
             intake.setIntake(gamepad1.right_trigger, gamepad1.left_trigger, gamepad1.right_bumper);
             wobbleGoal.moveWobbleGoalPosition(gamepad2.dpad_left, gamepad2.dpad_right);
             wobbleGoal.moveWobbleGoalManipulator(gamepad2.x, gamepad2.b);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Shooter.java
@@ -39,11 +39,17 @@ public class Shooter {
         shooterMotor2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, Constants.SHOOTER_PID_COEFFICIENTS);
     }
 
-    public void setShooter(double distanceFromLine)
+    public void autoSetShooter(double distanceFromLine)
     {
         double shooterSpeed = Math.min(Math.max((-(Math.pow(distanceFromLine, 4)/24)) + (Math.pow(distanceFromLine, 3)/4) + (Math.pow(distanceFromLine, 2)/24) + ((7*distanceFromLine)/4) + 195, Constants.MIN_SHOOTER_VELOCITY), Constants.MAX_SHOOTER_VELOCITY);
         //double shooterSpeed = Math.min(Math.max(184.688 + (10.1651 * (Math.pow(Math.E, 0.208726 * distanceFromLine))), Constants.MIN_SHOOTER_VELOCITY), Constants.MAX_SHOOTER_VELOCITY);
 
+        shooterMotor1.setVelocity(shooterSpeed, AngleUnit.DEGREES);
+        shooterMotor2.setVelocity(shooterSpeed, AngleUnit.DEGREES);
+    }
+
+    public void setShooter(double shooterSpeed)
+    {
         shooterMotor1.setVelocity(shooterSpeed, AngleUnit.DEGREES);
         shooterMotor2.setVelocity(shooterSpeed, AngleUnit.DEGREES);
     }
