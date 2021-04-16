2e65a4ef7b4fc3552062de0e285cdd64c1610617
Samarth Rao <raosamarth91@gmail.com>
2021-04-15 17:58:55 -0700

    Made all hardware names transient

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -9,20 +9,20 @@ import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 @Config
 public class Constants {
 
-    public static String CAMERA_NAME = "Webcam 1";
+    public static transient String CAMERA_NAME = "Webcam 1";
     public static int CAMERA_RESOLUTION_WIDTH = 1280;
     public static int CAMERA_RESOLUTION_HEIGHT = 720;
 
     // Drive Base Motor Names
-    public static String FRONT_LEFT_DRIVE_NAME = "fl";
-    public static String FRONT_RIGHT_DRIVE_NAME = "fr";
-    public static String REAR_LEFT_DRIVE_NAME = "rl";
-    public static String REAR_RIGHT_DRIVE_NAME = "rr";
+    public static transient String FRONT_LEFT_DRIVE_NAME = "fl";
+    public static transient String FRONT_RIGHT_DRIVE_NAME = "fr";
+    public static transient String REAR_LEFT_DRIVE_NAME = "rl";
+    public static transient String REAR_RIGHT_DRIVE_NAME = "rr";
 
     // Odometery Pod Names
-    public static String VERTICAL_LEFT_NAME = FRONT_LEFT_DRIVE_NAME;
-    public static String VERTICAL_RIGHT_NAME = FRONT_RIGHT_DRIVE_NAME;
-    public static String HORIZONTAL_NAME = REAR_LEFT_DRIVE_NAME;
+    public static transient String VERTICAL_LEFT_NAME = FRONT_LEFT_DRIVE_NAME;
+    public static transient String VERTICAL_RIGHT_NAME = FRONT_RIGHT_DRIVE_NAME;
+    public static transient String HORIZONTAL_NAME = REAR_LEFT_DRIVE_NAME;
 
     public static Vector2d GOAL_VECTOR2D = new Vector2d(145, 0);
 
@@ -33,10 +33,10 @@ public class Constants {
 
     public static double POWER_SHOT_VELOCITY = 217 * (8.0/10.0);
 
-    public static String SHOOTER_1_NAME = "s1";
-    public static String SHOOTER_2_NAME = "s2";
+    public static transient String SHOOTER_1_NAME = "s1";
+    public static transient String SHOOTER_2_NAME = "s2";
 
-    public static String KICKER_NAME = "kicker";
+    public static transient String KICKER_NAME = "kicker";
 
     public static double KICKER_OPEN_POS = 0.13;
     public static double KICKER_KICK_POS = 0.28;
@@ -44,14 +44,14 @@ public class Constants {
     public static PIDFCoefficients SHOOTER_PID_COEFFICIENTS = new PIDFCoefficients(10, 1, 2, 9);
 
     // Intake Constants
-    public static String INTAKE_TOP_NAME = "i1";
-    public static String INTAKE_BOTTOM_NAME = "i2";
+    public static transient String INTAKE_TOP_NAME = "i1";
+    public static transient String INTAKE_BOTTOM_NAME = "i2";
 
-    public static String LEFT_WALL_NAME = "lw";
-    public static String RIGHT_WALL_NAME = "rw";
+    public static transient String LEFT_WALL_NAME = "lw";
+    public static transient String RIGHT_WALL_NAME = "rw";
 
-    public static String LEFT_FUNNEL_NAME = "lf";
-    public static String RIGHT_FUNNEL_NAME = "rf";
+    public static transient String LEFT_FUNNEL_NAME = "lf";
+    public static transient String RIGHT_FUNNEL_NAME = "rf";
 
     public static double LEFT_FUNNEL_RELEASE_POS = 0.5;
     public static double RIGHT_FUNNEL_RELEASE_POS = 0.5;
@@ -63,13 +63,13 @@ public class Constants {
     public static double RIGHT_WALL_POS_IN = 0.6;
 
     // Wobble Goal Constants
-    public static String WOBBLE_GOAL_SERVO1_NAME = "wg1";
-    public static String WOBBLE_GOAL_SERVO2_NAME = "wg2";
-    public static String AUTO_WOBBLE_CLAW = "awg";
+    public static transient String WOBBLE_GOAL_SERVO1_NAME = "wg1";
+    public static transient String WOBBLE_GOAL_SERVO2_NAME = "wg2";
+    public static transient String AUTO_WOBBLE_CLAW = "awg";
 
     public static double[] WOBBLE_GOAL_POSITION_VALUES = {0.25, 0.5, 0.68};
 
-    public static String WOBBLE_GOAL_MANIPULATOR_SERVO = "wgm";
+    public static transient String WOBBLE_GOAL_MANIPULATOR_SERVO = "wgm";
     public static double WOBBLE_GOAL_MANIPULATOR_SERVO_OPEN_POS = 1.0;
     public static double WOBBLE_GOAL_MANIPULATOR_SERVO_CLOSE_POS = 0;
 }
