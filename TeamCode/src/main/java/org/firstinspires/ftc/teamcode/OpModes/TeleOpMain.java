f7f76e59fd0608c26f3e150a0d74bec4d9ecac80
Sidd Rao <siddrao.926@gmail.com>
2021-10-21 23:02:34 -0700

    Removed Unused Imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleOpMain.java
@@ -1,23 +1,11 @@
  package org.firstinspires.ftc.teamcode.OpModes;
-        import com.acmerobotics.dashboard.FtcDashboard;
-        import com.acmerobotics.roadrunner.geometry.Pose2d;
-        import com.acmerobotics.roadrunner.geometry.Vector2d;
-        import com.acmerobotics.roadrunner.trajectory.Trajectory;
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-        import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-        import org.firstinspires.ftc.robotcore.external.Telemetry;
-        import org.firstinspires.ftc.teamcode.Constants;
-
-        import org.firstinspires.ftc.teamcode.Systems.DriveBase.drive.MecanumDrive;
-
-        import static org.firstinspires.ftc.teamcode.Systems.DriveBase.drive.DriveConstants.*;
-
-        import java.util.List;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
  @TeleOp(name = "TeleOp - Main")
 public class TeleOpMain extends LinearOpMode {
-     
+
      @Override
      public void runOpMode() {
 
